ef686f523c6a1a89ab1f36d09c72ad46139156fc
Martin Lucina <martin@lucina.net>
2019-09-27 16:55:08 +0200

    First working version

--- unikernel.ml
+++ unikernel.ml
@@ -1,4 +1,5 @@
 open Lwt.Infix
+open Type
 
 module Main
     (KV: Mirage_kv_lwt.RO)
@@ -8,6 +9,11 @@ module Main
     let dst, dst_port = S.TCPV4.dst flow in
     Logs.info (fun f -> f "[%a:%d] New connection"
                   Ipaddr.V4.pp dst dst_port)
+
+  let log_read_err flow e =
+    let dst, dst_port = S.TCPV4.dst flow in
+    Logs.warn (fun f -> f "[%a:%d] Read error: %a, closing connection"
+                  Ipaddr.V4.pp dst dst_port S.TCPV4.pp_error e)
 
   let log_write_err flow e =
     let dst, dst_port = S.TCPV4.dst flow in
@@ -19,33 +25,98 @@ module Main
     Logs.info (fun f -> f "[%a:%d] Closing connection"
                   Ipaddr.V4.pp dst dst_port)
 
+  let write_string flow s =
+    S.TCPV4.write flow (Cstruct.of_string s)
+
+  let draw_screen flow screen =
+    let rec aux n =
+      let (Character_height h) = Screen.height screen in
+      if n < h then (
+        let text = Screen.get_line_at screen (Character_y (n + 1)) in
+        write_string flow text >>= function
+          | Error e -> (* TODO: We should terminate execution here *)
+              log_write_err flow e; S.TCPV4.close flow
+          | Ok () ->
+              aux (n + 1))
+      else Lwt.return_unit
+    in
+    aux 0
+
+  let draw_interpreter flow interpreter : unit Lwt.t =
+    let screen = Interpreter.screen interpreter in
+    let state = Interpreter.state interpreter in
+    let input = Interpreter.input interpreter in
+    let has_new_output = Interpreter.has_new_output interpreter in
+    if state = Interpreter.Waiting_for_input then
+      draw_screen flow (Screen.fully_scroll (Screen.print screen input))
+    else if has_new_output then
+      let screen_to_draw =
+        if Screen.needs_more screen then
+          Screen.more screen
+        else
+          screen in
+      draw_screen flow screen_to_draw
+    else
+      Lwt.return_unit
+
   let start kv s : unit Lwt.t =
 
-    let rec session_step flow interpreter =
-      let new_interpreter = match Interpreter.state interpreter with
-      | Interpreter.Waiting_for_input ->
-          S.TCPV4.close flow >|= fun () -> None
-      | Interpreter.Halted ->
-          S.TCPV4.close flow >|= fun () -> None
-      | Interpreter.Running ->
-          Lwt.return (Some (Interpreter.step interpreter))
-      in
-      new_interpreter >>= function
-      | None -> Lwt.return_unit
-      | Some new_interpreter -> session_step flow new_interpreter
+    (* Run a single Interpreter step *)
+    let rec session_step flow interpreter input : unit Lwt.t =
 
+      let read_more_input () =
+        S.TCPV4.read flow >>= function
+          | Ok `Eof ->
+              Lwt.return None
+          | Error e ->
+              log_read_err flow e; Lwt.return None
+          | Ok (`Data b) ->
+              let s = Cstruct.to_string b in
+              let input : char array = Array.make (String.length s) ' ' in
+              String.iteri (fun i c -> input.(i) <- c) s;
+              Lwt.return (Some (Array.to_list input))
+      in
+
+      let run = match Interpreter.state interpreter with
+      | Interpreter.Running ->
+          Lwt.return (Some (Interpreter.step interpreter, input))
+      | Interpreter.Waiting_for_input ->
+          begin
+            match input with
+            | [] ->
+              read_more_input () >>= begin function
+                | None -> Lwt.return None
+                | Some (input) -> Lwt.return (Some (interpreter, input))
+              end
+            | ch :: tl ->
+              Lwt.return (Some (Interpreter.step_with_input interpreter ch, tl))
+          end
+      | Interpreter.Halted ->
+          Lwt.return None
+      in
+
+      run >>= function
+        | None ->
+            log_closing flow; S.TCPV4.close flow
+        | Some (new_interpreter, input) ->
+            draw_interpreter flow new_interpreter >>= fun () ->
+            session_step flow new_interpreter input
+
+    (* Handle a new connection; setup the Interpreter *)
     and session_start kv flow =
       log_new flow;
       let get_story_content filename =
         KV.get kv (Mirage_kv.Key.v filename) >|= function
-        | Error e -> failwith "error"
-        | Ok file -> file
+        | Error _e ->
+            failwith "could not get story from KV"
+        | Ok file ->
+            file
       in
       get_story_content "MiniZork.Z3" >>= fun story_content ->
       let story = Story.load story_content in
       let screen = Screen.make (Character_height 50) (Character_width 80) in
       let interpreter = Interpreter.make story screen in
-      session_step flow interpreter
+      session_step flow interpreter []
 
     in
     let port = Key_gen.port () in
