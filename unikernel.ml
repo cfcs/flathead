217ec01b9ef80996704eefaf7717afc565094c67
Martin Lucina <martin@lucina.net>
2019-09-27 19:18:59 +0200

    TELNET support
    lib_telnet/ shamelessly stolen from @hannesm, thank you!

--- unikernel.ml
+++ unikernel.ml
@@ -26,7 +26,8 @@ module Main
                   Ipaddr.V4.pp dst dst_port)
 
   let write_string flow s =
-    S.TCPV4.write flow (Cstruct.of_string s)
+    (* Logs.info (fun f -> f "Output: %S" s); *)
+    S.TCPV4.write flow (Server.encode (Cstruct.of_string s))
 
   let draw_screen flow screen =
     let rec aux n =
@@ -62,34 +63,54 @@ module Main
   let start kv s : unit Lwt.t =
 
     (* Run a single Interpreter step *)
-    let rec session_step flow interpreter input : unit Lwt.t =
+    let rec session_step flow telnet interpreter input : unit Lwt.t =
 
-      let read_more_input () =
+      let rec read_more_input telnet =
         S.TCPV4.read flow >>= function
           | Ok `Eof ->
               Lwt.return None
           | Error e ->
               log_read_err flow e; Lwt.return None
           | Ok (`Data b) ->
-              let s = Cstruct.to_string b in
-              let input : char array = Array.make (String.length s) ' ' in
-              String.iteri (fun i c -> input.(i) <- c) s;
-              Lwt.return (Some (Array.to_list input))
+              let telnet, events, out = Server.handle telnet b in
+              S.TCPV4.write flow out >>= function
+                | Error e ->
+                    log_write_err flow e; Lwt.return None
+                | Ok () ->
+                    let read_characters = List.fold_left (fun acc event ->
+                      match event with
+                      | `Resize (_w, _h) ->
+                          acc (* TODO *)
+                      | `Data bytes ->
+                          let s = Cstruct.to_string bytes in
+                          let chars = Array.make (String.length s) ' ' in
+                          String.iteri (fun i c -> chars.(i) <- c) s;
+                          Array.to_list chars @ acc) [] events
+                    in
+                    match read_characters with
+                    | [] ->
+                        read_more_input telnet
+                    | _ ->
+                        Lwt.return (Some (telnet, read_characters))
       in
 
       let run = match Interpreter.state interpreter with
       | Interpreter.Running ->
-          Lwt.return (Some (Interpreter.step interpreter, input))
+          Lwt.return (Some (telnet, Interpreter.step interpreter, input))
       | Interpreter.Waiting_for_input ->
           begin
             match input with
             | [] ->
-              read_more_input () >>= begin function
+              read_more_input telnet >>= begin function
                 | None -> Lwt.return None
-                | Some (input) -> Lwt.return (Some (interpreter, input))
+                | Some (telnet, input) ->
+                    (* Logs.info (fun f -> f
+                      "Input: %a" Fmt.(list ~sep:(unit ",") char) input); *)
+                    Lwt.return (Some (telnet, interpreter, input))
               end
             | ch :: tl ->
-              Lwt.return (Some (Interpreter.step_with_input interpreter ch, tl))
+              Lwt.return (Some
+                (telnet, Interpreter.step_with_input interpreter ch, tl))
           end
       | Interpreter.Halted ->
           Lwt.return None
@@ -98,12 +119,12 @@ module Main
       run >>= function
         | None ->
             log_closing flow; S.TCPV4.close flow
-        | Some (new_interpreter, input) ->
+        | Some (new_telnet, new_interpreter, input) ->
             draw_interpreter flow new_interpreter >>= fun () ->
-            session_step flow new_interpreter input
+            session_step flow new_telnet new_interpreter input
 
     (* Handle a new connection; setup the Interpreter *)
-    and session_start kv flow =
+    and session_start kv flow telnet =
       log_new flow;
       let get_story_content filename =
         KV.get kv (Mirage_kv.Key.v filename) >|= function
@@ -116,14 +137,19 @@ module Main
       let story = Story.load story_content in
       let screen = Screen.make (Character_height 50) (Character_width 80) in
       let interpreter = Interpreter.make story screen in
-      session_step flow interpreter []
+      session_step flow telnet interpreter []
 
     in
     let port = Key_gen.port () in
     Logs.info (fun m -> m "Listening on [%a:%d]"
                   Fmt.(list Ipaddr.V4.pp) (S.(IPV4.get_ip @@ ipv4 s)) port);
     S.listen_tcpv4 s ~port (fun flow ->
-      session_start kv flow
+      let telnet, out = Server.init () in
+      S.TCPV4.write flow out >>= function
+        | Error e ->
+            log_write_err flow e; S.TCPV4.close flow
+        | Ok () ->
+            session_start kv flow telnet
     );
 
     S.listen s
